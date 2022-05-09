5400caaebf333bb3d7f32fdf0d3ca29ecd3b646d
Kris Nóva <kris@nivenly.com>
2022-05-08 21:00:21 -0400

    Automatic commit from github.com/kris-nova/bin/git-save
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- pkg/libxpid/libxpid_bpf.go
+++ pkg/libxpid/libxpid_bpf.go
@@ -26,6 +26,15 @@ import (
 	"unsafe"
 )
 
+const (
+	Empty8   string = "        "
+	Empty16  string = Empty8 + Empty8
+	Empty32  string = Empty16 + Empty16
+	Empty64  string = Empty32 + Empty32
+	Empty128 string = Empty64 + Empty64
+	Empty256 string = Empty128 + Empty128
+)
+
 // TODO We need to see what pid details we can get out of the kernel
 
 var libxpidbpfenummtx sync.Mutex
@@ -33,7 +42,7 @@ var libxpidbpfenummtx sync.Mutex
 func BPFMapType(mapType int) string {
 	libxpidbpfenummtx.Lock()
 	defer libxpidbpfenummtx.Unlock()
-	var name string
+	name := Empty256
 	cname := C.CString(name)
 	C.bpf_map_type_enum(C.int(mapType), cname)
 	defer C.free(unsafe.Pointer(cname))
